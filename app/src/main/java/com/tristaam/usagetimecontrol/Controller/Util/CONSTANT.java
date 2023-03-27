560ec5b6b3a590d9b64a3d1e06b3f5f71471e812
TrisTaam <yadartcon@gmail.com>
2023-03-27 23:33:18 +0700

    Add some CONSTANT

--- app/src/main/java/com/tristaam/usagetimecontrol/Controller/Util/CONSTANT.java
+++ app/src/main/java/com/tristaam/usagetimecontrol/Controller/Util/CONSTANT.java
@@ -1,6 +1,14 @@
 package com.tristaam.usagetimecontrol.Controller.Util;
 
 public class CONSTANT {
+    public static String INTENT_1 = "InstalledAppList1";
+    public static String INTENT_2 = "InstalledAppList2";
+    public static String INTENT_3 = "ChooseApp";
+    public static int REQUEST_CODE = 1;
     public static int START_DP = 70;
     public static int END_DP = 270;
+    public static int MIN_HOUR = 0;
+    public static int MIN_MINUTE = 0;
+    public static int MAX_HOUR = 24;
+    public static int MAX_MINUTE = 60;
 }
