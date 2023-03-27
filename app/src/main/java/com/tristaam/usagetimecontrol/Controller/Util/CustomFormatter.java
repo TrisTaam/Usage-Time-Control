b539564bf6ba473ead2ed2112d2347b2877fee5c
TrisTaam <yadartcon@gmail.com>
2023-03-27 23:33:47 +0700

    Add time formatter (HH:MM)

--- app/src/main/java/com/tristaam/usagetimecontrol/Controller/Util/CustomFormatter.java
+++ app/src/main/java/com/tristaam/usagetimecontrol/Controller/Util/CustomFormatter.java
@@ -2,10 +2,17 @@ package com.tristaam.usagetimecontrol.Controller.Util;
 
 import android.widget.NumberPicker;
 
-public class CustomFormatter implements NumberPicker.Formatter {
+public class CustomFormatter {
+    public static class NumberPickerCustomFormatter implements NumberPicker.Formatter {
+        @Override
+        public String format(int value) {
+            return String.format("%02d", value);
+        }
+    }
 
-    @Override
-    public String format(int value) {
-        return String.format("%02d", value);
+    public static String MilliSecToHHMM(long milliSec) {
+        long hour = milliSec / (60 * 60 * 1000);
+        long minute = (milliSec % (60 * 60 * 1000)) / (60 * 1000);
+        return String.format("%02d giờ %02d phút", hour, minute);
     }
 }
